538f6c126397fad8fed1b74f73421b094f9df5ac
Lautaro Colazo <lautarocolazo1@gmail.com>
2024-03-19 12:29:30 -0600

    success page

--- src/components/Layout/Pages/SignupSuccessPage.js
+++ src/components/Layout/Pages/SignupSuccessPage.js
@@ -1,11 +1,17 @@
+import React from "react";
 import styles from "./SignupSuccessPage.module.css";
 
 const SignupSuccessPage = () => {
   return (
-    <h2 className={styles.signupSuccess}>
-      You created your account successfully! Navigate to the login page{" "}
-      <a href="/">here</a>
-    </h2>
+    <>
+      <h2 className={styles.signupSuccess}>
+        Congratulations! Your account has been successfully created. You can now
+        proceed to the login page to access your account.
+      </h2>
+      <h2 className={styles.signupSuccessLink}>
+        <a href="/">Login</a>
+      </h2>
+    </>
   );
 };
 
